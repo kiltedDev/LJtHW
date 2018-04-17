f05553ede5ed50a8e50a190f71429fab5c39b7e5
Thomas Wilson <twilson.spqr@gmail.com>
2018-04-17 00:19:36 -0400

    Ch005 Drills

--- CreatingVariables.java
+++ CreatingVariables.java
@@ -1,20 +1,24 @@
 public class CreatingVariables {
   public static void main( String[] args ) {
-    int x, y, age;
-    double seconds, e, checking;
-    String firstName, lastName, title;
+    int x, y, age, ravens;
+    double seconds, e, checking, runes;
+    String firstName, lastName, title, secondName, thirdName;
 
     x = 10;
     y = 400;
     age = 39;
+    ravens = 2;
 
     seconds = 4.71;
     e = 2.71828182845904523536;
     checking = 1.89;
+    runes = 27;
 
     firstName = "Graham";
     lastName = "Mitchell";
     title = "Mr.";
+    secondName = "As-High";
+    thirdName = "Third";
 
     System.out.println( "The variable x contains " + x );
     System.out.println( "The value " + y + " is stored in the variable y.");
@@ -22,5 +26,7 @@ public class CreatingVariables {
     System.out.println( "A favorite irrational # is Euler's number " + e );
     System.out.println( "Hopefully you have more than $" + checking + "!" );
     System.out.println( "My name is " + title + " " + firstName + " " + lastName );
+    System.out.println( "I am called Highest, " + secondName + ", and " + thirdName + "." );
+    System.out.println( "I have " + ravens + " ravens and have learned " + runes + " secrets." );
   }
 }
