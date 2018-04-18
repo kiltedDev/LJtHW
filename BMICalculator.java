8080602f2aba498b63f553dac285435520ff2edc
Thomas Wilson <twilson.spqr@gmail.com>
2018-04-18 04:01:09 -0400

    Ch009 Drills

--- BMICalculator.java
+++ BMICalculator.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class BMICalculator {
   public static void main( String[] args) {
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, bmi;
+    double m, kg, bmi, inches, feet, h, w;
 
-    System.out.print( "your height in meters: " );
-    m = keyboard.nextDouble();
+    System.out.println( "Your height? " );
+    System.out.print( "Feet: " );
+    feet = keyboard.nextDouble();
+    System.out.print( "Inches: " );
+    inches = keyboard.nextDouble();
 
-    System.out.print( "Your mass in kg: " );
-    kg = keyboard.nextDouble();
+    h = inches + (feet*12);
 
-    bmi = kg / (m*m);
+    System.out.print( "Your weight in lbs: " );
+    w = keyboard.nextDouble();
+
+    bmi = w / (h*h);
 
     System.out.println( "Your MBI is: " + bmi );
   }
 }
+
+/* The drill asked for a conversion to imperial
+measurements, which feels dirty to me.  It does
+test my ability to do math with variables on my
+own though. */
