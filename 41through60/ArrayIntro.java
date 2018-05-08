1eefacf052dbb50be544357f3e1569d365270e4b
Thomas Wilson <twilson.spqr@gmail.com>
2018-05-08 19:40:53 -0400

    Ch048 Drills

--- 41through60/ArrayIntro.java
+++ 41through60/ArrayIntro.java
@@ -1,9 +1,16 @@
 public class ArrayIntro {
   public static void main( String[] args ) {
     String[] planets = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+    String[] moons = {"Luna", "Phobos", "Deimos", "Io", "Europa", "Ganymede", "Callisto", "Iapetus", "Titan", "Dione", "Hyperion", "Rhea", "Titania", "Oberon", "Triton"};
 
     for (String p : planets ) {
       System.out.println( p + "\t" + p.toUpperCase() );
+    }
+
+    System.out.println("\n\n");
+    
+    for (String m : moons ) {
+      System.out.println( m + "\t" + m.toLowerCase() );
     }
   }
 }
